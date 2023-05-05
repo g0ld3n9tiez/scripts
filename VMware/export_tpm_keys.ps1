c3277247a1810e974e6cd6faa68e05f117fa367f
g0ld3n9tiez <127542531+g0ld3n9tiez@users.noreply.github.com>
2023-05-05 15:46:07 +0200

    Create export_tpm_keys.ps1

---
+++ VMware/export_tpm_keys.ps1
@@ -0,0 +1,17 @@
+Import-Module VMware.PowerCLI
+# Change vCenter
+Connect-VIServer vcenter
+
+$VMHosts = get-vmhost | Sort-Object
+
+foreach ($VMHost in $VMHosts) {
+    $esxcli = Get-EsxCli -VMHost $VMHost
+    try {
+        $key = $esxcli.system.settings.encryption.recovery.list()
+        Write-Host "$VMHost;$($key.RecoveryID);$($key.Key)"
+    }
+
+    catch {
+        
+    }
+}
